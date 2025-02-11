640f1416e1a2aa43d96a905a1e1d3a933b0f6437
ZhangWanshi <wanshizhang77@gmail.com>
2025-02-11 23:08:50 +0000

    Initial commit

---
+++ src/main/java/com/wanshi/movies_review/Repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.wanshi.movies_review.Repository;
+
+import com.wanshi.movies_review.Entity.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
---
+++ src/test/java/com/wanshi/movies_review/MoviesReviewApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wanshi.movies_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class MoviesReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
